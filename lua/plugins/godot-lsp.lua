3f8d0c9de063b4c147fc7c93ac2b72e177302e7a
Johnny <johnnytrungle@gmail.com>
2025-06-17 21:48:11 +1000

    update godot

--- lua/plugins/godot-lsp.lua
+++ lua/plugins/godot-lsp.lua
@@ -2,14 +2,19 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      setup = {
-        gdscript = function(_, _)
-          require("lspconfig").gdscript.setup({
-            name = "godot",
-            cmd = vim.lsp.rpc.connect("127.0.0.1", "6005"),
-          })
-          return true
-        end,
+      servers = {
+        -- We're defining a server manually, so don't install with Mason
+        gdscript = {
+          -- `name` is not standard in lspconfig, but harmless
+          name = "godot",
+          -- use manual connection to the Godot editor
+          cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
+          -- optionally add filetypes and root_dir for better integration
+          filetypes = { "gd", "gdscript" },
+          root_dir = function(fname)
+            return vim.fn.getcwd() -- or use lspconfig.util.root_pattern
+          end,
+        },
       },
     },
   },
