be5bb800f5cc87488247f0354c0b0e9119025378
Johnny <johnnytrungle@gmail.com>
2025-07-18 16:17:36 +1000

    add godot

--- lua/plugins/godot-dap.lua
+++ lua/plugins/godot-dap.lua
@@ -17,6 +17,13 @@ return {
         project = "${workspaceFolder}",
         launch_scene = true,
       },
+      {
+        type = "godot",
+        request = "attach",
+        name = "Attach to running Godot instance",
+        host = "127.0.0.1",
+        port = 6006,
+      },
     }
   end,
 }
