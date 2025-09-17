76daec183985178deec1ae73a5bfe4db88ca0224
johnny <johnnytrungle@gmail.com>
2025-09-18 09:01:27 +1000

    add plugins

---
+++ lua/plugins/meowyarn.lua
@@ -0,0 +1,9 @@
+{
+    "retran/meow.yarn.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    config = function()
+        require("meow.yarn").setup({
+            -- Your custom configuration goes here
+        })
+    end,
+}
